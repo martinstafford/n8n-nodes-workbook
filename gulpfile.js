2edba1ddff998e13bcd70e806ca74a7e31fa0d3d
Martin Stafford <martin.stafford@orchard.com.au>
2025-09-01 16:37:40 +1000

    Stop gulp from corrupting icons

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
 	const nodeDestination = path.resolve('dist', 'nodes');
 
-	src(nodeSource).pipe(dest(nodeDestination));
+	src(nodeSource, {encoding: false}).pipe(dest(nodeDestination));
 
 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
 	const credDestination = path.resolve('dist', 'credentials');
